c03e0e40bcff939510eead4c57acd0d46ebaeeb1
ToxSCin <renatoserrano.am@gmail.com>
2023-03-02 16:00:50 -0300

    colocando +

--- Controller/Controller.php
+++ Controller/Controller.php
@@ -1,18 +1,17 @@
-<?php	
+<?php
 
 namespace ApiCep\Controller;
 
 abstract class Controller
 {
-    protected static function getResponseAsJSON($data)
+    public static function GetResponseAsJSON($data)
     {
-        header("Acess-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Origin: *");
         header("Content-type: application/json; charset=utf-8");
         header("Cache-Control: no-cache, must-revalidate");
-        header("Expires: Mom 26 Jul 1997 05:00:00 GMT");
+        header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
         header("Pragma: public");
-        
+
         exit(json_encode($data));
     }
 }
-
